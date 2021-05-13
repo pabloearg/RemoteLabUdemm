b617febdc11d234db257e87cd9fdb46d09f71dd7
pabloearg <pabloearg@gmail.com>
2021-05-13 01:54:10 -0300

    Fresh App

---
+++ ios/Pods/Target Support Files/Pods-RemoteLab-tvOSTests/Pods-RemoteLab-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RemoteLab_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_RemoteLab_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-RemoteLab/Pods-RemoteLab-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RemoteLab : NSObject
+@end
+@implementation PodsDummy_Pods_RemoteLab
+@end
