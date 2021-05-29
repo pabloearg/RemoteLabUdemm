f0aba6dbbc70cc043431acbd9808c06ec5e59e47
pabloearg <pabloearg@gmail.com>
2021-05-29 15:51:32 -0300

    remove gitignore files

---
+++ src/screens/Splash.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  StyleSheet, Text, View, TouchableHighlight,
+} from 'react-native';
+
+const Splash = () => (
+  <View>
+    <Text />
+  </View>
+);
+
+const styles = StyleSheet.create({});
+
+export default Splash;
