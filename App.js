6ebeeb25c89d16de4c09330e7ec760f80e9ceac4
pabloearg <pabloearg@gmail.com>
2021-06-05 19:36:41 -0300

    added aws amplify

--- App.js
+++ App.js
@@ -5,8 +5,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { StatusBar } from 'react-native';
+import Amplify from 'aws-amplify';
 import RootStack from './src/Navigators/RootStack';
 import { navigatorRef } from './src/Navigators/navigator';
+import config from './src/aws-exports';
+
+Amplify.configure(config);
 
 const App = () => (
   <SafeAreaProvider>
